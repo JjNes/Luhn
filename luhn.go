d6ce4a9a99d8d206b1f304bae8407a8c7272f08e
Egor Nizovets <jnesapps@gmail.com>
2025-09-04 22:59:42 +0300

    fix luhn

--- luhn.go
+++ luhn.go
@@ -3,20 +3,19 @@ package luhn
 func IsValid(number int) bool {
 	sum := number % 10
 	number /= 10
-
-	for i := 0; number > 0; i++ {
+	even := true
+	for number > 0 {
 		digit := number % 10
 		number /= 10
-
-		if i%2 == 0 {
+		if even {
 			digit *= 2
 			if digit > 9 {
 				digit -= 9
 			}
 		}
 		sum += digit
+		even = !even
 	}
-
 	return sum%10 == 0
 }
 
@@ -25,23 +24,21 @@ func IsValidStr(number string) bool {
 	if n == 0 {
 		return false
 	}
-
 	sum := 0
+	even := false
 	for i := n - 1; i >= 0; i-- {
 		digit := int(number[i] - '0')
 		if digit < 0 || digit > 9 {
 			return false
 		}
-
-		if (n-1-i)%2 == 1 {
+		if even {
 			digit *= 2
 			if digit > 9 {
 				digit -= 9
 			}
 		}
-
 		sum += digit
+		even = !even
 	}
-
 	return sum%10 == 0
 }
